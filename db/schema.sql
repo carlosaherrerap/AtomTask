f8fd2929e21c572b4d2062eb712c10bf45d611d1
cuentaScorpio <scorpio.007.lp@gmail.com>
2026-06-10 21:16:11 -0500

    post

--- db/schema.sql
+++ db/schema.sql
@@ -113,10 +113,12 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
+DROP TRIGGER IF EXISTS update_users_updated_at ON users;
 CREATE TRIGGER update_users_updated_at
     BEFORE UPDATE ON users
     FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
 
+DROP TRIGGER IF EXISTS update_projects_updated_at ON projects;
 CREATE TRIGGER update_projects_updated_at
     BEFORE UPDATE ON projects
     FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
